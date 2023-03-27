7b401a3331fc9576e89386875cbda5cf32c30e6b
Clive <clivelo.kw@gmail.com>
2023-03-27 10:39:34 +0800

    Added additional test cases

--- dynamic programming/kadane_algorithm.py
+++ dynamic programming/kadane_algorithm.py
@@ -2,6 +2,9 @@
 # Space complexity: O(1)
 
 def kadane(nums):
+    if len(nums) == 0:
+        return 0
+
     max_current = max_global = nums[0]
     for i in range(1, len(nums)):
         max_current = max(nums[i], max_current + nums[i])
--- dynamic programming/tests/test_kadane.py
+++ dynamic programming/tests/test_kadane.py
@@ -14,6 +14,21 @@ class UnitTestCase(unittest.TestCase):
         result = 27
         self.assertEqual(test_function(nums), result, "test_basic_case2")
 
+    def test_empty(self):
+        nums = []
+        result = 0
+        self.assertEqual(test_function(nums), result, "test_empty")
+
+    def test_one_element_positive(self):
+        nums = [3]
+        result = 3
+        self.assertEqual(test_function(nums), result, "test_one_element_positive")
+
+    def test_one_element_negative(self):
+        nums = [-5]
+        result = -5
+        self.assertEqual(test_function(nums), result, "test_one_element_negative")
+
 
 if __name__ == "__main__":
     unittest.main()
